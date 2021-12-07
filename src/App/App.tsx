1e1c5d5f4d4552ae7458a2a858ed3459e28ede34
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2021-12-07 23:40:00 +0100

    setup

---
+++ src/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className='App'>
+			<h2>Hello from react app</h2>
+		</div>
+	);
+};
+export default App;
---
+++ src/render.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App/App';
+
+function render() {
+	ReactDOM.render(<App />, document.body);
+}
+
+render();
